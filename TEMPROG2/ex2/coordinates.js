cd7a8d1061c0e89e32a5921f351020a266662630
ALANALEJANDRORODRIGUEZHERNANDEZ <alanrdzzhdzz@gmail.com>
2024-02-21 09:43:42 -0600

    Se crea la carpeta EX2 para trabajar con TDD

---
+++ TEMPROG2/ex2/coordinates.js
@@ -0,0 +1,9 @@
+module.exports = function coordinate (arr){
+    //1.Si el arreglo esta vacio debo de regresar
+    //un arreglo vacio
+    if(arr.length === 0){
+        return [];
+    }
+    return [];
+    
+}
---
+++ TEMPROG2/ex2/coordinates.test.js
@@ -0,0 +1,9 @@
+const coordinates = require('./coordinates');
+describe('Mi primer suite', () => {
+
+    it('should return an empty array' , () => {
+        expect(coordinates([])).toStrictEqual([]);
+    });
+    it('sh')
+})
+;
